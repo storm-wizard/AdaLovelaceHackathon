8e20fab238d615182f0c734dcf85a34e112b388a
四月晴 <30656770+SiYue0211@users.noreply.github.com>
2022-10-11 18:32:27 +0100

    Update HammerFactory.java

--- products/factories/HammerFactory.java
+++ products/factories/HammerFactory.java
@@ -7,24 +7,14 @@ import products.materials.Steel;
 
 public class HammerFactory {
 
-
-    public static Item createLightSteelSledgeHammer() {
-        return new Steel(
-                new Hammer("sledge hammer", 25, 7));
-    }
-
     public static Item createHeavySteelSledgeHammer() {
         return new Steel(
-                new  Hammer("sledge hammer", 40, 14));
-    }
-
-    public static Item createLightTitaniumFramingHammer() {
-        return new Titanium(
-                new Hammer("framing hammer", 60, 1));
+                new Hammer("sledge hammer", 40, 14));
     }
 
     public static Item createLightSteelFramingHammer() {
         return new Steel(
                 new Hammer("framing hammer", 30, 3));
     }
+
 }
