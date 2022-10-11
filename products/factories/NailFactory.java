b80f6fc91b422bfdf7c3a750a7969836d802fc86
四月晴 <30656770+SiYue0211@users.noreply.github.com>
2022-10-11 18:55:29 +0100

    Update NailFactory.java

--- products/factories/NailFactory.java
+++ products/factories/NailFactory.java
@@ -7,36 +7,22 @@ import products.materials.Steel;
 
 public class NailFactory {
 
-    //Brass nails for a fancy brass roof
-    public static Item createSmallBrassRoofingNails(){
+    //   Brass nails for a fancy brass roof
+    public static Item createSmallBrassRoofingNails()   {
         return new Brass(
                 new Nails("roofing nails", 10.0, 20));
     }
 
-    public static Item createMediumBrassRoofingNails(){
+    public static Item createMediumBrassRoofingNails()   {
         return new Brass(
                 new Nails("roofing nails", 20.0, 50));
     }
 
-    public static Item createLargeBrassRoofingNails(){
-        return new Brass(
-                new Nails("roofing nails", 35.0, 100));
-    }
 
-    //Steel nails for framing
-    public static Item createSmallSteelFramingNails() {
-        return new Steel(
-                new Nails("framing nails", 15.0,50));
-    }
-
-    public static Item createMediumSteelFramingNails() {
-        return new Steel(
-                new Nails("framing nails",25.0,100));
-    }
 
     public static Item createLargeSteelFramingNails() {
-        return new Steel(
-                new Nails("framing nails",45,200));
+        return new Steel(   
+                new Nails("framing nails", 45, 200));
     }
 
 }
